5fa1a8fef516896aa1eba8f7b1bb9a13bde217a4
Cade Scroggins <git@cadejs.com>
2017-04-30 10:02:54 -0700

    search soundcloud link

--- src/jsx/PlayerInfo.jsx
+++ src/jsx/PlayerInfo.jsx
@@ -1,6 +1,8 @@
 import Inferno from 'inferno';
 import Component from 'inferno-component';
+import Svg from './svg.jsx';
 import '../scss/PlayerInfo.scss';
+import exitUp from '../svg/exit-up.svg';
 
 const originalTitle = document.title;
 
@@ -9,11 +11,17 @@ export default function PlayerInfo(props) {
   if (props.loading) return <div className="spinner" />;
   if (!props.title && !props.artist) return;
   document.title = `${props.title} by ${props.artist}`;
+  const searchQuery = encodeURIComponent(`${props.title} ${props.artist}`);
+  const searchLink = `https://soundcloud.com/search/sounds?q=${searchQuery}`;
 
   return (
     <div className="PlayerInfo">
-      <span className="PlayerInfo_artist">{ props.artist }</span>
-      <span className="PlayerInfo_title">{ props.title }</span>
+      <a href={ searchLink } target="_blank">
+        <span className="PlayerInfo_artist">{ props.artist }</span>
+        <span className="PlayerInfo_title">
+          { props.title }&nbsp;<Svg src={ exitUp } />
+        </span>
+      </a>
     </div>
   );
 };
