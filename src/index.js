e32782dec9e23b1e9a5730537bb075c8f835d59e
Cade Scroggins <git@cadejs.com>
2018-05-12 23:17:30 -0700

    build additions

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import preact from 'preact';
 import App from './js/App.jsx';
 
 document.getElementById('splash').remove();
-preact.render(<App />, document.getElementById('inferno-app'));
+preact.render(<App />, document.getElementById('app'));
